0a623ae28ca5c4182115c58b0b48839f36e069c8
Shiv Dhumal <shivanjalidhumal0@gmail.com>
2025-05-22 12:02:58 +0530

    added toolbar appwise

--- employe/urls.py
+++ employe/urls.py
@@ -1,34 +1,35 @@
 from django.contrib import admin
 from django.urls import path, include
-from appname.views import user_profiles_view
-from myapp.views import demo_profiles_view
-from django.urls import path
-from appname.views import overall_api_chart_data
-from myapp.views import overall_api_chart_data2
-from appname.views import export_all_requests_csv
 from django.conf import settings
 
-
-# Include Debug Toolbar URLs when in DEBUG mode
-if settings.DEBUG:
-    import debug_toolbar
+from appname.views import (
+    user_profiles_view,
+    overall_api_chart_data,
+    export_all_requests_csv,
+)
+from myapp.views import demo_profiles_view, overall_api_chart_data2
 
 urlpatterns = [
-    # Django Debug Toolbar URL
-    path('__debug__/', include(debug_toolbar.urls)),
-
-    # Your other URLs
+    # Your app URLs
     path('', include('appname.urls')),
-    path('silk/', include('silk.urls', namespace='silk')),  # Silk dashboard
+    path('myapp/', include('myapp.urls')),  # myapp routes
+
+    # Silk profiling URLs
+    path('silk/', include('silk.urls', namespace='silk')),
     path('silk/user_prof/', user_profiles_view, name='user_profiling'),
-    path('silk/demo_prof/', demo_profiles_view, name='demo_profiles'), 
+    path('silk/demo_prof/', demo_profiles_view, name='demo_profiles'),
 
-    path("api/overall-chart/", overall_api_chart_data, name="overall_api_chart_data"),
-    path("api/overall-chart2/", overall_api_chart_data2, name="overall_api_chart_data2"),
+    # API URLs
+    path('api/overall-chart/', overall_api_chart_data, name='overall_api_chart_data'),
+    path('api/overall-chart2/', overall_api_chart_data2, name='overall_api_chart_data2'),
 
-    # For myapp
-    path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),  # Include myapp routes
-
+    # Export CSV
     path('export/all/', export_all_requests_csv, name='export_all_requests_csv'),
+
+    # Admin
+    path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
